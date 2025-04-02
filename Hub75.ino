9482fab086a906e7be229f6449d7faa25271988d
Avilab <contact@avilab.fr>
2025-04-02 12:55:28 +0200

    Removed flickering

--- Hub75.ino
+++ Hub75.ino
@@ -1,67 +1,57 @@
 #define PROJECT "Hub75  "__DATE__ " " __TIME__
+#include <digitalWriteFast.h>
 /* Hub75 Manual */
 #define R0 40
-#define G0 41
-#define B0 42
+#define G0 42
+#define B0 41
 #define R1 43
-#define G1 44
-#define B1 45
+#define G1 45
+#define B1 44
 #define LA 46
 #define LB 47
 #define LC 48
 #define LD 49
 #define LE 50
 #define CL 51
-#define OE 52
-#define LT 53
+#define LT 52
+#define OE 53
+
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define YELLOW 3
+#define BLUE 4
+#define MAGENTA 5
+#define CYAN 6
+#define WHITE 7
+
+#define WIDTH 128
+#define HEIGHT 32
 
 int pins[]={CL};
-byte pixels[64][32];
+byte pixels[WIDTH][HEIGHT];
 void setup() {
   Serial.begin(9600);
   Serial.println("===========================");
   Serial.println(PROJECT);
 
-  pinMode(R0, OUTPUT);
-  pinMode(G0, OUTPUT);
-  pinMode(B0, OUTPUT);
-  pinMode(R1, OUTPUT);
-  pinMode(G1, OUTPUT);
-  pinMode(B1, OUTPUT);
-  pinMode(LA, OUTPUT);
-  pinMode(LB, OUTPUT);
-  pinMode(LC, OUTPUT);
-  pinMode(LD, OUTPUT);
-  pinMode(LE, OUTPUT);
-  pinMode(CL, OUTPUT);
-  pinMode(OE, OUTPUT);
-  pinMode(LT, OUTPUT);
-
-  /*
-  for(byte y=0;y<16;y++){
-    for(byte x=0;x<64;x++){
-      if(x>1 && x<7 && y>1 && y<7){
-        pixels[x][y]=5;
-        pixels[x][y+16]=4;
-      } else if(x>1 && x<7 && (y==1 || y==7)){
-        pixels[x][y]=5;
-        pixels[x][y+16]=4;
-      } else if(y>1 && y<7 && (x==1 || x==7)){
-        pixels[x][y]=5;
-        pixels[x][y+16]=4;
-      } else if(x+y>30 && x-y<=34){
-        pixels[x][y]=2;
-        pixels[x][y+16]=1;
-      } else{
-        pixels[x][y]=2;
-        pixels[x][y+16]=4;
-      }
-    }
-  }
-  */
+  pinModeFast(R0, OUTPUT);
+  pinModeFast(G0, OUTPUT);
+  pinModeFast(B0, OUTPUT);
+  pinModeFast(R1, OUTPUT);
+  pinModeFast(G1, OUTPUT);
+  pinModeFast(B1, OUTPUT);
+  pinModeFast(LA, OUTPUT);
+  pinModeFast(LB, OUTPUT);
+  pinModeFast(LC, OUTPUT);
+  pinModeFast(LD, OUTPUT);
+  pinModeFast(LE, OUTPUT);
+  pinModeFast(CL, OUTPUT);
+  pinModeFast(OE, OUTPUT);
+  pinModeFast(LT, OUTPUT);
 }
 void pix(byte x, byte y, byte col){
-  if(x>=0 && x<64 && y>=0 && y<32)
+  if(x>=0 && x<WIDTH && y>=0 && y<HEIGHT)
     pixels[x][y]=col;
 }
 void rect(byte x,byte y,byte w,byte h,byte col){
@@ -74,78 +64,73 @@ void rect(byte x,byte y,byte w,byte h,byte col){
 
 void scenery(bool day, byte pos){
   // grass
-  rect(0,16,64,16,4);
+  rect(0,HEIGHT/2,WIDTH,HEIGHT/2, GREEN);
   // sky
-  rect(0,0,64,16,day?2:0);
+  rect(0,0,WIDTH,HEIGHT/2, day?BLUE:BLACK);
   if(day){
-    rect(pos+1,2,6,4,5);
-    rect(pos+2,1,4,6,5);
+    rect(pos+1,2,6,4,YELLOW);
+    rect(pos+2,1,4,6,YELLOW);
   }else{
-    rect(pos+3,1,1,1,7);
-    rect(pos+2,1,1,4,7);
-    rect(pos+1,2,1,4,7);
-    rect(pos+3,4,1,1,7);
-    rect(pos+6,4,1,1,7);
-    rect(pos+2,5,5,1,7);
-    rect(pos+2,6,4,1,7);
+    rect(pos+3,1,1,1,WHITE);
+    rect(pos+2,1,1,4,WHITE);
+    rect(pos+1,2,1,4,WHITE);
+    rect(pos+3,4,1,1,WHITE);
+    rect(pos+6,4,1,1,WHITE);
+    rect(pos+2,5,5,1,WHITE);
+    rect(pos+2,6,4,1,WHITE);
   }
 }
 
 void addr(byte adr){
-  digitalWrite(LA, adr & 1);
-  digitalWrite(LB, adr & 2);
-  digitalWrite(LC, adr & 4);
-  digitalWrite(LD, adr & 8);
+  digitalWriteFast(LA, adr & 1);
+  digitalWriteFast(LB, adr & 2);
+  digitalWriteFast(LC, adr & 4);
+  digitalWriteFast(LD, adr & 8);
   //digitalWrite(LE, adr & 16);
 }
 void color(byte c1, byte c2){
-  digitalWrite(R0, c1 & 1);
-  digitalWrite(G0, c1 & 2);
-  digitalWrite(B0, c1 & 4);
-  digitalWrite(R1, c2 & 1);
-  digitalWrite(G1, c2 & 2);
-  digitalWrite(B1, c2 & 4);
+  digitalWriteFast(R0, c1 & RED);
+  digitalWriteFast(G0, c1 & GREEN);
+  digitalWriteFast(B0, c1 & BLUE);
+
+  digitalWriteFast(R1, c2 & RED);
+  digitalWriteFast(G1, c2 & GREEN);
+  digitalWriteFast(B1, c2 & BLUE);
 }
+int lum=300;
 void draw(){
+  digitalWrite(OE, HIGH);
   for(byte y=0;y<16;y++){
-    digitalWrite(OE, HIGH);
     addr(y);
-    for(byte x=0;x<64;x++){
+    for(byte x=0;x<WIDTH;x++){
       color(pixels[x][y], pixels[x][y+16]);
-      digitalWrite(CL, false);
-      digitalWrite(CL, true);
+      digitalWriteFast(CL, false);
+      digitalWriteFast(CL, true);
     }
-    digitalWrite(OE, LOW);
-    delayMicroseconds(50);
+    digitalWriteFast(LT, LOW);
+    delayMicroseconds(lum);
+    digitalWriteFast(LT, HIGH);
   }
-  digitalWrite(LT, LOW);
-  digitalWrite(LT, HIGH);
+  digitalWriteFast(OE, LOW);
 }
 
-byte x=1;
-byte y=1;
-byte dx=1;
-byte dy=1;
 byte t=1;
-void update(){
-  rect(x,y,4,4,t);
-  t++;
-  if(t==7) t=1;
-  x+=dx;
-  y+=dy;
-  if(x<1) dx=1;
-  if(y<1) dy=1;
-  if(x>59) dx=-1;
-  if(y>27) dy=-1;
-  rect(x,y,4,4,7);
-}
 bool day=true;
-void loop() {
+void update() {
   t++;
-  if(t==72){
-    t=0;
-    day=!day;
-  }
+  if(t==128+8) day=!day;
+  if(t==128+8) t=0;
+}
+void serialEvent() {
+  char s=Serial.read();
+  if(s<65) return;
+  Serial.print(s);
+  Serial.print(",");
+  lum=25*(s-65);
+  Serial.println(lum);
+}
+void loop() {
+  update();
   scenery(day,t-7);
   draw();
 }
