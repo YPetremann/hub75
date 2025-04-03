495ae926c5a9c967460b4039c1730db9b0a517d5
Avilab <contact@avilab.fr>
2025-04-03 16:59:23 +0200

    Game

--- Hub75.ino
+++ Hub75.ino
@@ -14,89 +14,87 @@ RGBmatrixPanel *matrix = new RGBmatrixPanel(A, B, C, D, CLK, LAT, OE, true, 64);
 #define setBrightness(x) fillScreen(0)
 #define clear()          fillScreen(0)
 #define show()           swapBuffers(true)
-#define Color(x,y,z)     Color444(x/16,y/16,z/16)
-
-#define LED_BLACK           0
-
-#define LED_RED_VERYLOW    (3 <<  11)
-#define LED_RED_LOW        (7 <<  11)
-#define LED_RED_MEDIUM     (15 << 11)
-#define LED_RED_HIGH       (31 << 11)
-
-#define LED_GREEN_VERYLOW  (1 <<  5)
-#define LED_GREEN_LOW      (15 << 5)
-#define LED_GREEN_MEDIUM   (31 << 5)
-#define LED_GREEN_HIGH     (63 << 5)
-
-#define LED_BLUE_VERYLOW     3
-#define LED_BLUE_LOW         7
-#define LED_BLUE_MEDIUM     15
-#define LED_BLUE_HIGH       31
-
-#define LED_ORANGE_VERYLOW (LED_RED_VERYLOW + LED_GREEN_VERYLOW)
-#define LED_ORANGE_LOW     (LED_RED_LOW     + LED_GREEN_LOW)
-#define LED_ORANGE_MEDIUM  (LED_RED_MEDIUM  + LED_GREEN_MEDIUM)
-#define LED_ORANGE_HIGH    (LED_RED_HIGH    + LED_GREEN_HIGH)
-
-#define LED_PURPLE_VERYLOW (LED_RED_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_PURPLE_LOW     (LED_RED_LOW     + LED_BLUE_LOW)
-#define LED_PURPLE_MEDIUM  (LED_RED_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_PURPLE_HIGH    (LED_RED_HIGH    + LED_BLUE_HIGH)
-
-#define LED_CYAN_VERYLOW   (LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_CYAN_LOW       (LED_GREEN_LOW     + LED_BLUE_LOW)
-#define LED_CYAN_MEDIUM    (LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_CYAN_HIGH      (LED_GREEN_HIGH    + LED_BLUE_HIGH)
-
-#define LED_WHITE_VERYLOW  (LED_RED_VERYLOW + LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_WHITE_LOW      (LED_RED_LOW     + LED_GREEN_LOW     + LED_BLUE_LOW)
-#define LED_WHITE_MEDIUM   (LED_RED_MEDIUM  + LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_WHITE_HIGH     (LED_RED_HIGH    + LED_GREEN_HIGH    + LED_BLUE_HIGH)
+#define Color(x,y,z)     (x/16)<<11&(y/16)<<5&(z/16)
 
 #define SCREEN_WIDTH  64
 #define SCREEN_HEIGHT 32
 
-String text[4];
-int ofst[4];
-int minx[4];
-void setup() {
-  Serial.begin(9600);  // Initialiser la communication série à 9600 bauds
-  matrix->begin();
-  matrix->setTextWrap(false);
-  matrix->setBrightness(BRIGHTNESS);
-  matrix->setTextColor(LED_GREEN_HIGH);
-  matrix->setTextSize(1);
-  matrix->clear();
-  matrix->show();
-  setText(0,"Welcome");
-  setText(1,"to");
-  setText(2,"the");
-  setText(3,"Avilab");
-}
-void setText(int i, String txt){
-  text[i]=txt;
-  ofst[i]=SCREEN_WIDTH;
-  minx[i]=txt.length()*-6;
-}
-void scrollText() {
-  matrix->setTextWrap(false);
-  matrix->setTextSize(1);
-  matrix->setRotation(0);
-  for (int i=0; i<4; i++) {
-    matrix->setCursor(ofst[i],i*8);
-    matrix->print(text[i]);
-    ofst[i]=ofst[i]<minx[i] ? SCREEN_WIDTH : ofst[i]-1 ;
-  }
-}
+struct Pos {
+  uint8_t x;
+  uint8_t y;
+};
+struct Target {
+  int color;
+  Pos a;
+  Pos b;
+};
+struct Dot {
+  Pos o;
+  Pos n;
+};
 
-int index=0;
-void loop() {
-  if (Serial.available()) {
-    setText(index,Serial.readString());
-    index=(index+1)%4;
-  }
-  matrix->clear();
-  scrollText();
+Target targ;
+Target trap;
+Dot dot;
+
+void setup() {
+  Serial.begin(9600);
+  matrix->begin();
+  matrix->fillScreen(Color(255,255,0));
+  matrix->setBrightness(BRIGHTNESS);
   matrix->show();
-  delay(1);
+  dot.n.x=16;
+  dot.n.y=16;
+  nextTarget();
+}
+uint8_t clamp(uint8_t min,uint8_t cur,uint8_t max){
+  if(cur<=min) return min;
+  if(cur>=max) return max;
+  return cur;
+}
+int score=-1;
+void nextTarget(){
+  matrix->fillScreen(0);
+  do{
+    targ.a.x=random(3)*11; targ.b.x=targ.a.x+10;
+    targ.a.y=random(3)*11; targ.b.y=targ.a.y+10;
+  } while(check(targ));
+  do{
+    trap.a.x=random(3)*11; trap.b.x=trap.a.x+10;
+    trap.a.y=random(3)*11; trap.b.y=trap.a.y+10;
+  } while(check(trap) || (trap.a.x==targ.a.x && trap.a.y==targ.a.y));
+  
+  matrix->fillRect(targ.a.x,targ.a.y,targ.b.x-targ.a.x,targ.b.y-targ.a.y,31<<5);
+  matrix->fillRect(trap.a.x,trap.a.y,trap.b.x-trap.a.x,trap.b.y-trap.a.y,31<<11);
+  score++;
+  matrix->drawLine(32,0,32,32,0xFFFF);
+  matrix->setCursor(34, 0);
+  matrix->setTextColor(0xFFFF);
+  matrix->print(score);
+}
+bool check(Target to){
+  return dot.n.x>=to.a.x && dot.n.x<=to.b.x && dot.n.y>=to.a.y && dot.n.y<=to.b.y;
+}
+void gameOver(){
+  matrix->fillRect(0,0,32,32,0xFFFF);
+  matrix->show();
+  delay(100);
+  score=-1;
+  matrix->fillScreen(0);
+  nextTarget();
+}
+int TRAIL=Color(255,255,0);
+int HEAD=Color(255,255,255);
+void loop() {
+  matrix->drawLine(dot.o.x,dot.o.y,dot.n.x,dot.n.y,0);
+  dot.o.x=dot.n.x;
+  dot.o.y=dot.n.y;
+  dot.n.x = 31-(analogRead(A9)*32/1024);
+  dot.n.y = analogRead(A10)*32/1024;
+  matrix->drawLine(dot.o.x,dot.o.y,dot.n.x,dot.n.y,31);
+  matrix->drawPixel(dot.n.x,dot.n.y,0xFFFF);
+  if(check(targ)) nextTarget();
+  if(check(trap)) gameOver();
+  matrix->show();
+  delay(100);
 }
