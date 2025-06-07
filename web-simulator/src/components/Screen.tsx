223a06cdcd892a283960ba231edbac17074e473c
YPetremann <y.petremann@gmail.com>
2025-06-07 05:52:36 +0200

    Add renderGuide prop to Screen component for cursor display control

--- web-simulator/src/components/Screen.tsx
+++ web-simulator/src/components/Screen.tsx
@@ -1,15 +1,17 @@
 import type React from "react";
 import { useEffect, useRef } from "react";
+import { displayCursor } from "../rendered/doCommand";
 
 type ScreenProps = {
 	renderData: string;
+	renderGuide: boolean;
 	renderFrame: (ctx: CanvasRenderingContext2D, renderData: string) => void;
 };
 
 const CANVAS_WIDTH = 640;
 const CANVAS_HEIGHT = 320;
 
-const Screen: React.FC<ScreenProps> = ({ renderData, renderFrame }) => {
+const Screen: React.FC<ScreenProps> = ({ renderData, renderGuide, renderFrame }) => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
 	useEffect(() => {
@@ -19,6 +21,19 @@ const Screen: React.FC<ScreenProps> = ({ renderData, renderFrame }) => {
 		if (!ctx) return;
 		renderFrame(ctx, renderData);
 	}, [renderData, renderFrame]);
+	useEffect(()=>{
+		if (!renderGuide) return;
+		// call displayCursor every 250ms
+		const interval = setInterval(displayCursor, 250);
+		return () => {
+			clearInterval(interval);
+			const canvas = canvasRef.current;
+			if (!canvas) return;
+			const ctx = canvas.getContext("2d");
+		if (!ctx) return;
+			renderFrame(ctx, renderData);
+		}
+	},[renderGuide])
 	return (
 		<div className="flex flex-col items-center justify-center">
 			<canvas
